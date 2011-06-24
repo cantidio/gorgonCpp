80bee8a5db107343014984f6e1b6e013a5855eba
Cantidio Fontes <aniquilatorbloody@gmail.com>
2011-06-24 08:33:48 +0000

    - Tinha esquecido de adicionar esse arquivo antes

---
+++ include/input/mouse_button.hpp
@@ -0,0 +1,46 @@
+/*
+ *
+ *    __     ___   _ __    __     ___     ___
+ *  /'_ `\  / __`\/\`'__\/'_ `\  / __`\ /' _ `\
+ * /\ \L\ \/\ \L\ \ \ \//\ \L\ \/\ \L\ \/\ \/\ \
+ * \ \____ \ \____/\ \_\\ \____ \ \____/\ \_\ \_\
+ *  \/___L\ \/___/  \/_/ \/___L\ \/___/  \/_/\/_/
+ *    /\____/              /\____/
+ *    \_/__/               \_/__/
+ *
+ *  Copyright (C) 2008-2011  Cantidio Oliveira Fontes
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+#ifndef _GORGON_INPUT_MOUSE_BUTTON_
+#define _GORGON_INPUT_MOUSE_BUTTON_
+
+namespace Gorgon{
+namespace Input
+{
+	struct MouseButton
+	{
+		enum Id
+		{
+			LEFT,
+			MIDDLE,
+			RIGHT,
+			WHEEL_UP,
+			WHEEL_DOWN,
+			LAST_BUTTON
+		};/**<< enum that stores the buttons of the mouse*/
+	};
+}}
+#endif
